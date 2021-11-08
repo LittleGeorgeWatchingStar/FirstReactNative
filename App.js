4f01e29163552d9791f923eee60b02f7338585e8
LittleGeorgeWatchingStar <quanzhangubc2019@gmail.com>
2021-11-08 11:28:57 -0800

    start on viewItem page

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 export default function App() {
   return (
     <NativeBaseProvider>
-      <ListingDetailsScreen />
+      {/* <ListingDetailsScreen /> */}
+      <ViewImageScreen />
     </NativeBaseProvider>
   );
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 
 import colors from "../config/colors";
 
@@ -8,8 +9,12 @@ import AppText from "../components/AppText";
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" style={styles.closeIconImage} size={32}></MaterialCommunityIcons>
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons name="trash-can-outline" style={styles.closeIconImage} size={32}></MaterialCommunityIcons>
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -21,21 +26,24 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
+    // width: 50,
+    // height: 50,
+    // backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
+  },
+  closeIconImage: {
+    color: colors.white,
   },
   container: {
     backgroundColor: colors.black,
     flex: 1,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
+    // width: 50,
+    // height: 50,
+    // backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
