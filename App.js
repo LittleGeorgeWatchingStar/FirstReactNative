51fac2cff01df4a60c36ffeb1c8cbc3b1fdceba3
LittleGeorgeWatchingStar <quanzhangubc2019@gmail.com>
2021-11-09 15:23:37 -0800

    add components

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }  from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -7,13 +7,34 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <NativeBaseProvider>
       {/* <ListingDetailsScreen /> */}
       {/* <ListingsScreen /> */}
-      <MessagesScreen />
+      {/* <AccountScreen /> */}
+      <Screen>
+        <AppPicker
+          selectedItem={category}
+          onSelectItem={(item) => setCategory(item)}
+          items={categories}
+          icon="apps"
+          placeholder="Category"
+        />
+        <AppTextInput icon="email" placeholder="Email" />
+      </Screen>
     </NativeBaseProvider>
   );
 }
